93cfc86803ebebc20328858f77ee5cb6a3393c60
IRINA BATYCHKO <mironenkoi@yandex.ru>
2023-04-02 00:41:38 +0300

    Выполнено второе домашнее задание.

---
+++ src/main/java/homework2/MainClassTask2.java
@@ -0,0 +1,24 @@
+package homework2;
+/*Задача №2.
+Напишите метод, который составит строку, состоящую из 100 повторений слова TEST и метод,
+который запишет эту строку в простой текстовый файл, обработайте исключения.*/
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class MainClassTask2 {
+    public static void main(String[] args) {
+        test();
+    }
+
+    private static void test() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("TEST ".repeat(100));
+
+        try (PrintWriter pw = new PrintWriter("src/main/resources/output.txt")) {
+            pw.print(sb);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
